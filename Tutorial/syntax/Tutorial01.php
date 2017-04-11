2834954ffef7ca8d2016ab1bc069eaa8df69b342
liwenhui <lwh1131@outlook.com>
2017-04-11 22:58:42 +0800

    2017/04/11

---
+++ Tutorial/syntax/Tutorial01.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Syntax</title>
+</head>
+<body>
+<?php
+echo "my first php<br/>";
+$x=5/*+15*/+5;
+echo "$x<br/>";
+$abc="hello php";
+Echo "hello word<br/>";
+echo "hello word<br/>";
+ECHO "hello word<br/>";
+echo "$abc<br/>";
+echo "$Abc<br/>";
+echo "$ABC<br/>";
+?>
+</body>
+</html>

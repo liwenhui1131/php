05dcb0b5c73474e0cbe168acff08ea7337b5acd8
liwenhui <lwh1131@outlook.com>
2017-04-14 22:57:52 +0800

    2017/04/14

--- Tutorial/syntax.php
+++ Tutorial/syntax.php
@@ -8,9 +8,8 @@
 
 <?php
 $a = 'hello word';
-echo "php want to say $a<br/>";
-echo "php say" . $a . "<br/>";
-    
+echo "php want to say $a<br>";
+echo "php say" . $a . "<br>";
 echo "<h2>variables</h2>";
 $b = 1;
 $d = 3;
@@ -19,10 +18,10 @@ function showVariables()
 {
     $c = 2;
     global $d;
-    echo "local变量C的值为:$c<br/>";
-    echo "global变量b的值为:$b<br/>";
-    echo "global变量b的值为:$d<br/>";
-    echo "blobal变量b的值为:" . $GLOBALS['e'] . "<br/>";
+    echo "local变量C的值为:$c<br>";
+    echo "global变量b的值为:$b<br>";
+    echo "global变量b的值为:$d<br>";
+    echo "blobal变量b的值为:" . $GLOBALS['e'] . "<br>";
 }
 
 showVariables();
@@ -30,14 +29,13 @@ function staticVariable()
 {
     static $f = 5;
     $f++;
-    echo "static变量的值为：$f<br/>";
+    echo "static变量的值为：$f<br>";
 }
 
 staticVariable();
 staticVariable();
 staticVariable();
-    
-echo "<h2>data types</h2>";    
+echo "<h2>data types</h2>";
 var_dump($int = 1);
 echo '<br/>';
 var_dump($flo = 2.1);
@@ -60,25 +58,23 @@ class car
 }
 
 $herbie = new car();
-echo $herbie->model . '<br/>';
+echo $herbie->model . '<br>';
 var_dump($herbie);
-    
-echo "<h2>strings</h2>";    
+echo "<h2>strings</h2>";
 $str = "hello word";
-echo "字符串str的值为:" . $str . "<br/>";
-echo "str的字符串长度为:" . strlen($str) . "<br/>";
-echo "str的字数为:" . str_word_count($str) . "<br/>";
-echo "reverse字符串str的结果为:" . strrev($str) . "<br/>";
-echo "字符串str中word字符的位置在:" . strpos($str, 'word') . "<br/>";
-echo "用liwenhui替换字符串str中的word:" . str_replace('word', 'liwenhui', $str) . "<br/>";
-    
-echo "<h2>constants</h2>";   
+echo "字符串str的值为:" . $str . "<br>";
+echo "str的字符串长度为:" . strlen($str) . "<br>";
+echo "str的字数为:" . str_word_count($str) . "<br>";
+echo "reverse字符串str的结果为:" . strrev($str) . "<br>";
+echo "字符串str中word字符的位置在:" . strpos($str, 'word') . "<br>";
+echo "用liwenhui替换字符串str中的word:" . str_replace('word', 'liwenhui', $str) . "<br>";
+echo "<h2>constants</h2>";
 define("name", "liwenhui");
-echo "常量name的值为：" . name . "<br/>";
-echo "常量name的值为：" . NAME . "<br/>";
+echo "常量name的值为：" . name . "<br>";
+echo "常量name的值为：" . NAME . "<br>";
 define("age", 233, true);
-echo "常量age的值为：" . age . "<br/>";
-echo "常量age的值为：" . AGE . "<br/>";
+echo "常量age的值为：" . age . "<br>";
+echo "常量age的值为：" . AGE . "<br>";
 ?>
 
 </body>
---
+++ Tutorial/syntax1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+$a = 12;
+$b = 13;
+if ($a == 12 xor $b == 13) {
+    echo "两个条件为真<br>";
+}
+if ($a == 12 xor $b == 14) {
+    echo "两个条件有一个为真<br>";
+}
+$c = "abc";
+$d = "efg";
+$f = $c . $d;
+echo "f的值为:$f<br>";
+$c .= $d;
+echo "c的值为:$c<br>";
+$abc = 12;
+if ($abc < 0) {
+    echo "abc的值小于0<br>";
+} elseif ($abc < 10) {
+    echo "abc的值大于0且小于10<br>";
+} else {
+    echo "abc的值大于10<br>";
+}
+switch ($abc) {
+    case 10:
+        echo "abc的值为10<br>";
+        break;
+    case 11:
+        echo "abc的值为11<br>";
+        break;
+    case 12:
+        echo "abc的值为12<br>";
+        break;
+}
+?>
+</body>
+</html>

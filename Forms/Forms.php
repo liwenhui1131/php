ba4a231be6763d270623822ffa8f477c2ede4b95
liwenhui <lwh1131@outlook.com>
2017-04-20 22:53:54 +0800

    2017/04/20

---
+++ Forms/Forms.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Forms</title>
+</head>
+<body>
+<form method="post" action="Forms.php">
+
+    Name: <input type="text" name="name">
+    <span class="error">* <?php echo $nameErr; ?></span>
+    <br><br>
+    E-mail:
+    <input type="text" name="email">
+    <span class="error">* <?php echo $emailErr; ?></span>
+    <br><br>
+    Website:
+    <input type="text" name="website">
+    <span class="error"><?php echo $websiteErr; ?></span>
+    <br><br>
+    Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+    <br><br>
+    Gender:
+    <input type="radio" name="gender" value="female">Female
+    <input type="radio" name="gender" value="male">Male
+    <span class="error">* <?php echo $genderErr; ?></span>
+    <br><br>
+    <input type="submit" name="submit" value="Submit">
+
+</form>
+
+Welcome <?php echo $_POST['name']; ?><br>
+Your email address is: <?php echo $_POST['email']; ?>
+
+<?php
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+    } else {
+        $name = test_input($_POST["name"]);
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+    }
+
+    if (empty($_POST["website"])) {
+        $website = "";
+    } else {
+        $website = test_input($_POST["website"]);
+    }
+
+    if (empty($_POST["comment"])) {
+        $comment = "";
+    } else {
+        $comment = test_input($_POST["comment"]);
+    }
+
+    if (empty($_POST["gender"])) {
+        $genderErr = "Gender is required";
+    } else {
+        $gender = test_input($_POST["gender"]);
+    }
+}
+?>
+</body>
+</html>

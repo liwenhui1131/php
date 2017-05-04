f0ee65571c6bca13a0818b63a369d63eebc35d91
liwenhui <lwh1131@outlook.com>
2017-05-04 22:58:03 +0800

    2017/05/04

---
+++ MySQL Database/01MySQL_connect.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MySQL Connect</title>
+</head>
+<body>
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully" . "<br>";
+
+$conn->close();
+?>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+$conn = mysqli_connect($servername, $username, $password);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully" . "<br>";
+
+mysqli_close($conn)
+?>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+$conn = null;
+?>
+</body>
+</html>
---
+++ MySQL Database/02Create_DB.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MySQL Create DB</title>
+</head>
+<body>
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+$conn = new mysqli($servername, $username, $password);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error . "<br>";
+}
+
+$conn->close();
+?>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+$conn = mysqli_connect($servername, $username, $password);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "CREATE DATABASE myDB";
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($conn) . "<br>";
+}
+
+mysqli_close($conn);
+?>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE DATABASE myDBPDO";
+    $conn->exec($sql);
+    echo "Database created successfully<br>";
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage() . "<br>";
+}
+
+$conn = null;
+?>
+</body>
+</html>
